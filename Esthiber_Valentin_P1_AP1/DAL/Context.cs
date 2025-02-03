b4670174c2f520d0bbef0317828fb331af0a8336
Esthiber <esthiber1@gmail.com>
2025-02-03 18:49:18 -0400

    Creacion del Contexto.

---
+++ Esthiber_Valentin_P1_AP1/DAL/Context.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Esthiber_Valentin_P1_AP1.DAL
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+
+        public DbSet<Models.Modelo> Modelos { get; set; }
+    }
+}
